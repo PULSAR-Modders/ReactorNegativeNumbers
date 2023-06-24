ba4955c8e38669db9d893e3fd41d7f391ef96138
18107 <18107@users.noreply.github.com>
2023-06-24 19:17:10 +0930

    Add files via upload

---
+++ EngineerReactorScreen.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Reactor_Negative_Numbers
+{
+    [HarmonyPatch(typeof(PLEngineerReactorScreen), "UpdateCoreTempSection")]
+    internal class EngineerReactorScreen
+    {
+        static void Postfix(PLEngineerReactorScreen __instance, UIWidget ___TempBarOuter, PLReactor ___MyReactor, UILabel ___StabilityBarValueLabel, PLCachedFormatString<int> ___cStabilityBarValueLabel)
+        {
+            if (___TempBarOuter != null && ___MyReactor != null)
+            {
+                PLGlobal.SafeLabelSetText(___StabilityBarValueLabel, ___cStabilityBarValueLabel.ToString(Mathf.FloorToInt((1f - __instance.MyScreenHubBase.OptionalShipInfo.CoreInstability) * 100f)));
+            }
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Reactor_Negative_Numbers
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Displays negative reactor stability instead of 0 when the stability is below 0";
+
+        public override string Name => "Reactor negative numbers";
+
+        public override string ModID => "reactornegativenumbers";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.reactornegativenumbers";
+        }
+    }
+}
